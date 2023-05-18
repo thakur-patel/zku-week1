9dfb5ee087598fc7c99cbf12a9178b81f09c10ac
thakur <thakur@patel.com>
2023-05-18 15:29:49 +0530

    completed Pt.3 Q3

--- Q3/contracts/bonus/SystemOfEquations.circom
+++ Q3/contracts/bonus/SystemOfEquations.circom
@@ -1,7 +1,7 @@
 pragma circom 2.0.0;
 
 include "../../node_modules/circomlib/circuits/comparators.circom";
-include ""; // hint: you can use more than one templates in circomlib-matrix to help you
+include "../../node_modules/circomlib-matrix/circuits/matMul.circom"; // hint: you can use more than one templates in circomlib-matrix to help you
 
 template SystemOfEquations(n) { // n is the number of variables in the system of equations
     signal input x[n]; // this is the solution to the system of equations
@@ -10,6 +10,49 @@ template SystemOfEquations(n) { // n is the number of variables in the system of
     signal output out; // 1 for correct solution, 0 for incorrect solution
 
     // [bonus] insert your code here
+    signal prod[n];
+    signal bool[n];
+
+    component product = matMul(n,n,1);
+    component eq[n];
+
+    product.a <== A;
+
+    for (var i=0; i < n; i++){
+        product.b[i][0] <== x[i];
+    }
+    
+    var total = 0;
+
+    for (var i=0; i<n; i++){
+        eq[i] = IsEqual();
+        eq[i].in[0] <== b[i];
+        eq[i].in[1] <== product.out[i][0];
+        bool[i] <== eq[i].out;
+        
+        // THIS DOES NOT WORK
+        // b[i] === product.out[i][0];
+        // if(b[i] == product.out[i][0]){
+        //     total++;
+        // } 
+    }
+
+    signal boolProd[n];
+    boolProd[0] <== bool[0];
+
+    for (var i=1; i<n; i++){
+        boolProd[i] <== boolProd[i-1] * bool[i];
+    }
+    out <== boolProd[n-1];
+
+    // THIS DOES NOT WORK
+    // if(total == n){
+    //     out<==1;
+    // }
+    // else{
+    //     out<==0;
+    // }
+
 }
 
 component main {public [A, b]} = SystemOfEquations(3);
--- Q3/test/bonus-test.js
+++ Q3/test/bonus-test.js
@@ -53,62 +53,62 @@ describe("RangeProof circuit test", function () {
     });
 });
 
-// describe("SystemOfEquations circuit test", function () {
-//     this.timeout(100000000);
+describe("SystemOfEquations circuit test", function () {
+    this.timeout(100000000);
 
-//     it("Bonus question", async () => {
-//         const circuit = await wasm_tester("contracts/bonus/SystemOfEquations.circom");
+    it("Bonus question", async () => {
+        const circuit = await wasm_tester("contracts/bonus/SystemOfEquations.circom");
 
-//         const INPUT = {
-//             "x": ["15","17","19"],
-//             "A": [["1","1","1"],["1","2","3"],["2",Fr.e(-1),"1"]],
-//             "b": ["51", "106", "32"]
-//         }
+        const INPUT = {
+            "x": ["15","17","19"],
+            "A": [["1","1","1"],["1","2","3"],["2",Fr.e(-1),"1"]],
+            "b": ["51", "106", "32"]
+        }
 
-//         const witness = await circuit.calculateWitness(INPUT, true);
+        const witness = await circuit.calculateWitness(INPUT, true);
 
-//         //console.log(witness);
+        console.log(witness);
 
-//         assert(Fr.eq(Fr.e(witness[0]),Fr.e(1)));
-//         assert(Fr.eq(Fr.e(witness[1]),Fr.e(1)));
-//     });
-// });
+        assert(Fr.eq(Fr.e(witness[0]),Fr.e(1)));
+        assert(Fr.eq(Fr.e(witness[1]),Fr.e(1)));
+    });
+});
 
-// describe("SystemOfEquations verifier test", function () {
-//     let Verifier;
-//     let verifier;
+describe("SystemOfEquations verifier test", function () {
+    let Verifier;
+    let verifier;
 
-//     beforeEach(async function () {
-//         Verifier = await ethers.getContractFactory("SystemOfEquationsVerifier");
-//         verifier = await Verifier.deploy();
-//         await verifier.deployed();
-//     });
+    beforeEach(async function () {
+        Verifier = await ethers.getContractFactory("SystemOfEquationsVerifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed();
+    });
 
-//     it("Should return true for correct proof", async function () {
-//         //[assignment] Add comments to explain what each line is doing
-//         const { proof, publicSignals } = await groth16.fullProve({
-//             "x": ["15","17","19"],
-//             "A": [["1","1","1"],["1","2","3"],["2","-1","1"]],
-//             "b": ["51", "106", "32"]
-//         },
-//             "contracts/bonus/SystemOfEquations/SystemOfEquations_js/SystemOfEquations.wasm","contracts/bonus/SystemOfEquations/circuit_final.zkey");
+    it("Should return true for correct proof", async function () {
+        //[assignment] Add comments to explain what each line is doing
+        const { proof, publicSignals } = await groth16.fullProve({
+            "x": ["15","17","19"],
+            "A": [["1","1","1"],["1","2","3"],["2","-1","1"]],
+            "b": ["51", "106", "32"]
+        },
+            "contracts/bonus/SystemOfEquations/SystemOfEquations_js/SystemOfEquations.wasm","contracts/bonus/SystemOfEquations/circuit_final.zkey");
 
-//         const calldata = await groth16.exportSolidityCallData(proof, publicSignals);
+        const calldata = await groth16.exportSolidityCallData(proof, publicSignals);
     
-//         const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
+        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
     
-//         const a = [argv[0], argv[1]];
-//         const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
-//         const c = [argv[6], argv[7]];
-//         const Input = argv.slice(8);
+        const a = [argv[0], argv[1]];
+        const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
+        const c = [argv[6], argv[7]];
+        const Input = argv.slice(8);
 
-//         expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
-//     });
-//     it("Should return false for invalid proof", async function () {
-//         let a = [0, 0];
-//         let b = [[0, 0], [0, 0]];
-//         let c = [0, 0];
-//         let d = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-//         expect(await verifier.verifyProof(a, b, c, d)).to.be.false;
-//     });
-// });
+        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
+    });
+    it("Should return false for invalid proof", async function () {
+        let a = [0, 0];
+        let b = [[0, 0], [0, 0]];
+        let c = [0, 0];
+        let d = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        expect(await verifier.verifyProof(a, b, c, d)).to.be.false;
+    });
+});
